7e8ce312def2b3c3da55157e47b0487eb39b20f9
Ayomide <wilsonace87@gmail.com>
2023-01-06 10:14:16 +0100

    initialized the project

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+/* Configuration */
+const app = express();
