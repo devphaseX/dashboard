d5f781305ee94b29f3a4310faaa4bb57fb64af48
Ayomide <wilsonace87@gmail.com>
2023-01-09 19:12:35 +0100

    add the transaction model

---
+++ server/src/model/Transaction.ts
@@ -0,0 +1,16 @@
+import mongoose, { SchemaDefinition } from 'mongoose';
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    userId: String,
+    cost: String,
+    products: {
+      type: [mongoose.Types.ObjectId],
+      of: Number,
+    },
+  } satisfies SchemaDefinition,
+  { timestamps: true }
+);
+
+const Transaction = mongoose.model('Product', TransactionSchema);
+export { Transaction };
