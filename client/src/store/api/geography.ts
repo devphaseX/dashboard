fae0bb71d7351a62c7f971364df6938702d8e527
Ayomide <wilsonace87@gmail.com>
2023-01-10 11:24:44 +0100

    add the geography api

---
+++ client/src/store/api/geography.ts
@@ -0,0 +1,24 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { baseUrl, extractData } from './shared';
+
+interface GeographyData {
+  field: string;
+  value: string;
+}
+
+const api = createApi({
+  baseQuery: baseUrl,
+  tagTypes: ['Geography'],
+  reducerPath: 'adminGeographyApi',
+  endpoints(build) {
+    return {
+      getGeography: build.query<Array<GeographyData>, void>({
+        query: (_arg: void) => '/client/geography',
+        providesTags: ['Geography'],
+        transformResponse: extractData,
+      }),
+    };
+  },
+});
+
+export { api };
