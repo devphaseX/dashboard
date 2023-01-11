c1960ed2fc50276f113d000bbeb08e286f04a9f5
Ayomide <wilsonace87@gmail.com>
2023-01-11 11:01:40 +0100

    add a timestamp support

--- server/src/model/ProductStat.ts
+++ server/src/model/ProductStat.ts
@@ -1,23 +1,26 @@
 import mongoose, { SchemaDefinition } from 'mongoose';
 
-const ProductStatSchema = new mongoose.Schema({
-  productId: String,
-  yealySalesTotal: Number,
-  yearlyTotalSoldUnits: Number,
-  year: Number,
-  monthlyData: [
-    {
-      month: String,
+const ProductStatSchema = new mongoose.Schema(
+  {
+    productId: String,
+    yealySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: [
+      {
+        month: String,
+        totalSales: Number,
+        totalUnits: Number,
+      },
+    ],
+    dailyData: {
+      day: String,
       totalSales: Number,
       totalUnits: Number,
     },
-  ],
-  dailyData: {
-    day: String,
-    totalSales: Number,
-    totalUnits: Number,
-  },
-} satisfies SchemaDefinition);
+  } satisfies SchemaDefinition,
+  { timestamps: true }
+);
 
 const ProductStat = mongoose.model('ProductStat', ProductStatSchema);
 
